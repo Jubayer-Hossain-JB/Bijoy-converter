268c7672cf1d0cb984c269f58b41976d0c4bb89d
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-10-27 02:19:09 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@ self.addEventListener("install", (event) => {
       .open('website-cache')
       .then((cache) =>{
         cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/script.js',
-          "/icon.svg",
-          "/key.json",
-          "/sutonny.ttf",
+          '/Bijoy-converter/',
+          '/Bijoy-converter/index.html',
+          '/Bijoy-converter/style.css',
+          '/Bijoy-converter/script.js',
+          "/Bijoy-converter/icon.svg",
+          "/Bijoy-converter/key.json",
+          "/Bijoy-converter/sutonny.ttf",
           "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
           "https://fonts.googleapis.com/css2?family=Mulish:wght@400;600&family=Noto+Sans+Bengali&display=swap",
 
