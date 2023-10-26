93b4398496322c4301a71a9b4b3ae531f4828b30
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-10-27 01:26:27 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open('website-cache')
+      .then((cache) =>{
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          "/icon.svg",
+          "/key.json",
+          "/sutonny.ttf",
+          "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+          "https://fonts.googleapis.com/css2?family=Mulish:wght@400;600&family=Noto+Sans+Bengali&display=swap",
+
+        ])
+      }
+      ),
+  );
+});
+
+
+if(!navigator.onLine){
+  self.addEventListener('fetch', event => {
+      // Check if the requested file is in the cache.
+      event.respondWith(caches.match(event.request));
+    });
+}
