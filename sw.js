a87a6c46c62cb22ad588d95542e2d6ca29eeb368
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-11-06 22:05:08 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,13 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open('website-cache')
-      .then((cache) =>{
-        cache.addAll([
-          '/Bijoy-converter/',
-          '/Bijoy-converter/index.html',
-          '/Bijoy-converter/style.css',
-          '/Bijoy-converter/script.js',
-          "/Bijoy-converter/icon.svg",
-          "/Bijoy-converter/key.json",
-          "/Bijoy-converter/sutonny.ttf",
-          "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
-          "https://fonts.googleapis.com/css2?family=Mulish:wght@400;600&family=Noto+Sans+Bengali&display=swap",
-
-        ])
-      }
-      ),
-  );
-});
-
-
-
 self.addEventListener('fetch', event=>{
   event.respondWith(
     fetch(event.request)
+    .then(res=>{
+      var resClone = res.clone()
+      caches
+      .open('website-cache')
+      .then(cache=>cache.put(event.request, resClone))
+      return res
+    })
     .catch(_=>caches.match(event.request))
   )
 })
-
