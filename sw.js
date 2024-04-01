9381343cef1952ed8354a35c67d6b1ca35641f87
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2024-04-01 19:33:55 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
 self.addEventListener('fetch', event=>{
-  event.respondWith(
-    fetch(event.request)
-    .then(res=>{
-      var resClone = res.clone()
-      caches
-      .open('website-cache')
-      .then(cache=>cache.put(event.request, resClone))
-      return res
-    })
-    .catch(_=>caches.match(event.request))
-  )
+  if(event.request.method!="POST"){
+    event.respondWith(
+      fetch(event.request)
+      .then(res=>{
+        var resClone = res.clone()
+        caches
+        .open('website-cache')
+        .then(cache=>cache.put(event.request, resClone))
+        return res
+      })
+      .catch(_=>caches.match(event.request))
+    )
+  }
 })
