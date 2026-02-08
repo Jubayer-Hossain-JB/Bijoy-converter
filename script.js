5602746daa68fd7f5e8981997a785a5ffbf680cc
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2026-02-08 11:40:53 +0600

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -277,8 +277,9 @@ function asyncCall(e) {
                     }
                 }
             }
+            console.log(s)
+            return s
         }           
-        return s
     }
     function d(e, t, l = !1) {
         var s;
@@ -445,6 +446,7 @@ document.onkeydown = (e) => {
     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
     if (e.keyCode === 123 || ctrlShiftKey(e, "I") || ctrlShiftKey(e, "J") || ctrlShiftKey(e, "C") || (e.ctrlKey && e.keyCode === "U".charCodeAt(0))) return false;
 };
+
 
 
 
