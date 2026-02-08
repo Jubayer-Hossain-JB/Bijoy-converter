51818cfe7c5fc6603880dfcde6d6132be73aeb20
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2026-02-08 17:12:13 +0600

    Update the word 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -67,7 +67,6 @@ function getClusteredTextArray(element, result = []) {
 
       // Recurse into the element
       getClusteredTextArray(node, result);
-      }
     }
   }
   return result
@@ -411,6 +410,7 @@ document.onkeydown = (e) => {
     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
     if (e.keyCode === 123 || ctrlShiftKey(e, "I") || ctrlShiftKey(e, "J") || ctrlShiftKey(e, "C") || (e.ctrlKey && e.keyCode === "U".charCodeAt(0))) return false;
 };
+
 
 
 
